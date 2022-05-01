069835eb1df4fdcf6d0e112cd728ce40360a84a1
Luiz Eduardo <luiz.silva@phooto.com.br>
2022-05-01 18:59:52 -0300

    feat: entity auto validation

--- ddd/src/entity/customer.ts
+++ ddd/src/entity/customer.ts
@@ -1,20 +1,35 @@
 class Customer {
     _id: string;
     _name: string;
-    _address: string;
-    _active: boolean = true;
+    _address: string = '';
+    _active: boolean = false;
 
-    constructor (id: string, name: string, address: string) {
+    constructor (id: string, name: string) {
         this._id = id;
         this._name = name;
-        this._address = address;
+        this.validate();
+    }
+
+    validate() {
+        if (this._id.length === 0) {
+            throw new Error("Id is required");
+        }
+
+        if (this._name.length === 0) {
+            throw new Error("Name is required");
+        }
     }
 
     changeName(name: string) {
         this._name = name;
+        this.validate();
     }
 
     activate() {
+        if (this._address.length === 0) {
+            throw new Error("Address is mandatory to activate a customer");
+        }
+        
         this._active = true;
     }
 
