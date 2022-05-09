182cfb69fef225b3ece1384f7356e84d251fdd76
Luiz Eduardo <gs.luizeduardo00@gmail.com>
2022-05-09 18:49:37 -0300

    feat: created address value object

--- ddd/src/entity/customer.ts
+++ ddd/src/entity/customer.ts
@@ -10,10 +10,12 @@
  * - - customer.ts (get, set)
  */
 
+import Address from "./address";
+
 class Customer {
     _id: string;
     _name: string;
-    _address: string = '';
+    _address!: Address;
     _active: boolean = false;
 
     constructor (id: string, name: string) {
@@ -38,7 +40,7 @@ class Customer {
     }
 
     activate() {
-        if (this._address.length === 0) {
+        if (this._address === undefined) {
             throw new Error("Address is mandatory to activate a customer");
         }
         
@@ -47,5 +49,9 @@ class Customer {
 
     deactivate() {
         this._active = false;
+    }
+
+    set Address(address: Address) {
+        this._address = address;
     }
 }
