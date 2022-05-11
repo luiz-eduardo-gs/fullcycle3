c70985d7c2d2293f59c851e85515913f888f4eb9
Luiz Eduardo <gs.luizeduardo00@gmail.com>
2022-05-11 18:44:56 -0300

    feat: created order service tests

---
+++ ddd/src/service/order.service.spec.ts
@@ -0,0 +1,17 @@
+import Order from "../entity/order";
+import OrderItem from "../entity/order_item";
+
+describe("Order service unit tests", () => {
+    it("should return all orders total", () => {
+
+        const orderItem1 = new OrderItem('i1', 'Item 1', 100, 'p1', 1);
+        const orderItem2 = new OrderItem('i2', 'Item 2', 200, 'p2', 2);
+
+        const order1 = new Order('123', 'c1', [orderItem1]);
+        const order2 = new Order('124', 'c1', [orderItem2]);
+    
+        const total = OrderService.total([order1, order2]);
+
+        expect(total).toBe(500);
+    });
+})
