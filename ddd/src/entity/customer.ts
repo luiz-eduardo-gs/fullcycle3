ac3d8294e97d17fd3ce974b6e317169f1bd61c8c
Luiz Eduardo <gs.luizeduardo00@gmail.com>
2022-05-11 18:57:39 -0300

    feat: npm i uuid and created placeOrder method

--- ddd/src/entity/customer.ts
+++ ddd/src/entity/customer.ts
@@ -17,6 +17,7 @@ export default class Customer {
     private _name: string;
     private _address!: Address;
     private _active: boolean = false;
+    private _rewardPoints: number = 0;
 
     constructor (id: string, name: string) {
         this._id = id;
@@ -24,8 +25,16 @@ export default class Customer {
         this.validate();
     }
 
+    get id(): string {
+        return this._id;
+    }
+
     get name(): string {
         return this._name;
+    }
+
+    get rewardPoints(): number {
+        return this._rewardPoints;
     }
 
     validate() {
@@ -57,6 +66,10 @@ export default class Customer {
 
     deactivate() {
         this._active = false;
+    }
+
+    addRewardPoints(points: number) {
+        this._rewardPoints = points;
     }
 
     set Address(address: Address) {
