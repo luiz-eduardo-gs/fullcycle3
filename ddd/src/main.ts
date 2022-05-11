0f5289374f295a89a8cf81561b914720290d22f6
Luiz Eduardo <gs.luizeduardo00@gmail.com>
2022-05-11 18:41:26 -0300

    feat: created product service

--- ddd/src/main.ts
+++ ddd/src/main.ts
@@ -10,6 +10,6 @@ customer.Address = address;
 customer.activate();
 
 // Agregados
-const item1 = new OrderItem('1', 'Item 1', 10);
-const item2 = new OrderItem('2', 'Item 2', 15);
-const order = new Order('1', '123', [item1, item2]);
+// const item1 = new OrderItem('1', 'Item 1', 10);
+// const item2 = new OrderItem('2', 'Item 2', 15);
+// const order = new Order('1', '123', [item1, item2]);
