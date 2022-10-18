ad5aea561fe94f5b484ceb312681404b4233fe5b
Luiz Eduardo <gs.luizeduardo00@gmail.com>
2022-10-17 21:33:59 -0300

    feat: created Category aggregate

--- codeflix/java/fc3-admin-do-catalogo/application/src/main/java/com/luizeduardo/admin/catalogo/application/UseCase.java
+++ codeflix/java/fc3-admin-do-catalogo/application/src/main/java/com/luizeduardo/admin/catalogo/application/UseCase.java
@@ -1,7 +1,5 @@
 package com.luizeduardo.admin.catalogo.application;
 
-import com.luizeduardo.admin.catalogo.domain.Category;
-
 public class UseCase {
     public Category execute() {
         return new Category();
--- codeflix/java/fc3-admin-do-catalogo/domain/src/main/java/com/luizeduardo/admin/catalogo/domain/Category.java
+++
@@ -1,5 +0,0 @@
-package com.luizeduardo.admin.catalogo.domain;
-
-public class Category {
-    public String id;
-}
