5e8eb7669b29e8b669ef7942c28d5c1a7297ea1c
Luiz Eduardo <gs.luizeduardo00@gmail.com>
2022-10-20 13:02:03 -0300

    feat: activate and deactivate category

--- codeflix/java/fc3-admin-do-catalogo/domain/src/main/java/com/luizeduardo/admin/catalogo/domain/category/Category.java
+++ codeflix/java/fc3-admin-do-catalogo/domain/src/main/java/com/luizeduardo/admin/catalogo/domain/category/Category.java
@@ -43,6 +43,22 @@ public class Category extends AggregateRoot<CategoryID> {
         return id;
     }
 
+    public Category activate() {
+        this.deletedAt = null;
+        this.active = true;
+        this.updatedAt = Instant.now();
+        return this;
+    }
+
+    public Category deactivate() {
+        if (getDeletedAt() == null) {
+            this.deletedAt = Instant.now();
+        }
+        this.active = false;
+        this.updatedAt = Instant.now();
+        return this;
+    }
+
     @Override
     public void validate(final ValidationHandler handler) {
         new CategoryValidator(this, handler).validate();
