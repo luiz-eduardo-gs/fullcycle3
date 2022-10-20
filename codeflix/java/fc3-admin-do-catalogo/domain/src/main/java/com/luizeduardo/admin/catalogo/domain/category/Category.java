2290632c21f59c30189754654c1e249e2784295e
Luiz Eduardo <gs.luizeduardo00@gmail.com>
2022-10-20 13:22:39 -0300

    feat: update category

--- codeflix/java/fc3-admin-do-catalogo/domain/src/main/java/com/luizeduardo/admin/catalogo/domain/category/Category.java
+++ codeflix/java/fc3-admin-do-catalogo/domain/src/main/java/com/luizeduardo/admin/catalogo/domain/category/Category.java
@@ -59,6 +59,18 @@ public class Category extends AggregateRoot<CategoryID> {
         return this;
     }
 
+    public Category update(final String aName, final String aDescription, final boolean isActive) {
+        if (isActive) {
+            activate();
+        } else {
+            deactivate();
+        }
+        this.name = aName;
+        this.description = aDescription;
+        this.updatedAt = Instant.now();
+        return this;
+    }
+
     @Override
     public void validate(final ValidationHandler handler) {
         new CategoryValidator(this, handler).validate();
